a7aff0e00a4463bd1b0004720a89c1b9fc722186
trigkit4 <2550810279@qq.com>
2016-08-11 11:12:25 +0800

    Initial Commit

---
+++ Kkjrxiangmu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kkjrxiangmu
+//
+//  Created by 贾增辉 on 16/8/11.
+//  Copyright © 2016年 kkjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Kkjrxiangmu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Kkjrxiangmu
+//
+//  Created by 贾增辉 on 16/8/11.
+//  Copyright © 2016年 kkjr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
